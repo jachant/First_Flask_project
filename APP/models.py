ab340fe493ca91691469cd27c9f24179eef04a15
artyom-antonov <Jachant@yandex.ru>
2023-11-13 20:32:00 +0300

    Create user, check, for email and phone, validity, get user, via/users/<int:id>

---
+++ APP/models.py
@@ -0,0 +1,35 @@
+#model.py
+import re
+
+
+class User:
+    #todo: user class
+    def __init__(self, id, first_name, last_name, phone, email, score=0):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.score = score
+
+    @staticmethod
+    def is_valid_email(email):
+        if re.match(r"[^@]+@[^@]+\.[^@]+", email): return True
+        return False
+
+    @staticmethod
+    def is_valid_phone(phone):
+        if re.match(r"^\+?[1-9][0-9]{7,14}$", phone): return True
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
